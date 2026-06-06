e9a2c3e549ec0d247f980e5b5e79d9c6ea2e6ea2
fariswirakusuma <fariswkt@gmail.com>
2026-06-06 19:45:56 +0700

    fix: fixed vm

--- src/interpreter/VirtualMachine.cpp
+++ src/interpreter/VirtualMachine.cpp
@@ -1,200 +1,144 @@
 #include <interpreter/VirtualMachine.hpp>
-#include <interpreter/StackMemory.hpp>
 #include <iostream>
 
-VirtualMachine::VirtualMachine() {
-    instructionPointer = 0;
-    basePointer = 0;
-    stackPointer = -1;
-    stack.resize(MAX_STACK_SIZE, 0);
-}
+VirtualMachine::VirtualMachine() : instructionPointer(0) {}
 
 void VirtualMachine::loadCode(const std::vector<TACInstruction>& icgCode) {
     code = icgCode;
 }
 
-void VirtualMachine::checkStackOverflow(int increment) {
-    if (stackPointer + increment >= MAX_STACK_SIZE) {
-        throw std::runtime_error("Runtime Error: Stack Overflow.");
-    }
-}
-
-void VirtualMachine::checkStackUnderflow(int decrement) {
-    if (stackPointer - decrement < -1) {
-        throw std::runtime_error("Runtime Error: Stack Underflow.");
-    }
-}
-
-void VirtualMachine::checkMemoryBounds(int address) {
-    if (address < 0 || address >= MAX_STACK_SIZE) {
-        throw std::runtime_error("Runtime Error: Out-of-Bounds Memory Access.");
-    }
-}
-
 void VirtualMachine::run() {
     instructionPointer = 0;
-    StackMemory vmMem;
-    
+    memory.reset();
+
     while (instructionPointer < static_cast<int>(code.size())) {
         TACInstruction instr = code[instructionPointer];
         instructionPointer++;
-        
+
         if (instr.op == "LIT") {
-            checkStackOverflow(1);
-            stackPointer++;
-            stack[stackPointer] = instr.arg;
-        } 
+            // Push literal value onto stack
+            memory.push(instr.arg);
+        }
         else if (instr.op == "LOD") {
-            checkStackOverflow(1);
-            checkMemoryBounds(instr.arg);
-            stackPointer++;
-            stack[stackPointer] = stack[instr.arg];
-        } 
+            // Load value from absolute address
+            memory.push(memory.getValueAt(instr.arg));
+        }
         else if (instr.op == "STO") {
-            checkStackUnderflow(1);
-            checkMemoryBounds(instr.arg);
-            stack[instr.arg] = stack[stackPointer];
-            stackPointer--;
-        } 
+            // Pop top of stack and store at absolute address
+            int val = memory.pop();
+            memory.setValueAt(instr.arg, val);
+        }
         else if (instr.op == "INT") {
-            checkStackOverflow(instr.arg);
-            stackPointer += instr.arg;
-        } 
+            // Allocate space on stack (increment SP by arg)
+            memory.allocate(instr.arg);
+        }
         else if (instr.op == "JMP") {
             instructionPointer = instr.arg;
-        } 
+        }
         else if (instr.op == "JPC") {
-            checkStackUnderflow(1);
-            if (stack[stackPointer] == 0) {
+            // Jump if top of stack == 0 (false), then pop
+            int cond = memory.pop();
+            if (cond == 0) {
                 instructionPointer = instr.arg;
             }
-            stackPointer--;
-        } 
+        }
         else if (instr.op == "RET") {
-            if (basePointer == 0) return;
-        } 
+            if (memory.getBasePointer() == 0) return;
+        }
         else if (instr.op == "LODI") {
-            checkStackUnderflow(1);
-            int addr = stack[stackPointer];
-            checkMemoryBounds(addr);
-            stack[stackPointer] = stack[addr]; 
-        } 
+            // Indirect load: pop address, push value at that address
+            int addr = memory.pop();
+            memory.push(memory.getValueAt(addr));
+        }
         else if (instr.op == "STOI") {
-            checkStackUnderflow(2);
-            int val = stack[stackPointer];
-            int addr = stack[stackPointer - 1];
-            checkMemoryBounds(addr);
-            stack[addr] = val;
-            stackPointer -= 2;
+            // Indirect store: pop value, pop address, store value at address
+            int val  = memory.pop();
+            int addr = memory.pop();
+            memory.setValueAt(addr, val);
         }
         else if (instr.op == "OPR") {
             int val1, val2;
-            switch(instr.arg) {
-                case 1:
-                    checkStackUnderflow(0);
-                    stack[stackPointer] = -stack[stackPointer];
+            switch (instr.arg) {
+                case 1: // Unary negation
+                    val1 = memory.pop();
+                    memory.push(-val1);
                     break;
                 case 2: // ADD
-                    val2 = vmMem.pop();
-                    val1 = vmMem.pop();
-                    vmMem.push(val1 + val2);
+                    val2 = memory.pop();
+                    val1 = memory.pop();
+                    memory.push(val1 + val2);
                     break;
                 case 3: // SUB
-                    val2 = vmMem.pop();
-                    val1 = vmMem.pop();
-                    vmMem.push(val1 - val2);
+                    val2 = memory.pop();
+                    val1 = memory.pop();
+                    memory.push(val1 - val2);
                     break;
                 case 4: // MUL
-                    val2 = vmMem.pop();
-                    val1 = vmMem.pop();
-                    vmMem.push(val1 * val2);
+                    val2 = memory.pop();
+                    val1 = memory.pop();
+                    memory.push(val1 * val2);
                     break;
                 case 5: // DIV
-                    val2 = vmMem.pop();
-                    val1 = vmMem.pop();
-
-                    if (val2 == 0) throw std::runtime_error("Runtime Error: Modulus by zero.");
-                    
-                    vmMem.push(val1 / val2);
+                    val2 = memory.pop();
+                    val1 = memory.pop();
+                    if (val2 == 0) throw std::runtime_error("Runtime Error: Division by zero.");
+                    memory.push(val1 / val2);
                     break;
                 case 6: // MOD
-                    val2 = vmMem.pop();
-                    val1 = vmMem.pop();
-                    
+                    val2 = memory.pop();
+                    val1 = memory.pop();
                     if (val2 == 0) throw std::runtime_error("Runtime Error: Modulus by zero.");
-
-                    vmMem.push(val1 % val2);
+                    memory.push(val1 % val2);
                     break;
                 case 7: // EQL
-                    val2 = vmMem.pop();
-                    val1 = vmMem.pop();
-                    vmMem.push(val1 == val2);
+                    val2 = memory.pop();
+                    val1 = memory.pop();
+                    memory.push(val1 == val2 ? 1 : 0);
                     break;
                 case 8: // NEQ
-                    val2 = vmMem.pop();
-                    val1 = vmMem.pop();
-                    vmMem.push(val1 != val2);
+                    val2 = memory.pop();
+                    val1 = memory.pop();
+                    memory.push(val1 != val2 ? 1 : 0);
                     break;
                 case 9: // LSS
-                    val2 = vmMem.pop();
-                    val1 = vmMem.pop();
-                    vmMem.push(val1 < val2);
+                    val2 = memory.pop();
+                    val1 = memory.pop();
+                    memory.push(val1 < val2 ? 1 : 0);
                     break;
                 case 10: // GEQ
-                    val2 = vmMem.pop();
-                    val1 = vmMem.pop();
-                    vmMem.push(val1 >= val2);
+                    val2 = memory.pop();
+                    val1 = memory.pop();
+                    memory.push(val1 >= val2 ? 1 : 0);
                     break;
                 case 11: // GTR
-                    val2 = vmMem.pop();
-                    val1 = vmMem.pop();
-                    vmMem.push(val1 > val2);
+                    val2 = memory.pop();
+                    val1 = memory.pop();
+                    memory.push(val1 > val2 ? 1 : 0);
                     break;
                 case 12: // LEQ
-                    val2 = vmMem.pop();
-                    val1 = vmMem.pop();
-                    vmMem.push(val1 <= val2);
-                    // checkStackUnderflow(2);
-                    // val2 = stack[stackPointer--];
-                    // val1 = stack[stackPointer];
-                    // if (instr.arg == 2) stack[stackPointer] = val1 + val2;
-                    // else if (instr.arg == 3) stack[stackPointer] = val1 - val2;
-                    // else if (instr.arg == 4) stack[stackPointer] = val1 * val2;
-                    // else if (instr.arg == 5) {
-                    //     if (val2 == 0) throw std::runtime_error("Runtime Error: Division by zero.");
-                    //     stack[stackPointer] = val1 / val2;
-                    // }
-                    // else if (instr.arg == 6) {
-                    //     if (val2 == 0) throw std::runtime_error("Runtime Error: Modulus by zero.");
-                    //     stack[stackPointer] = val1 % val2;
-                    // }
-                    // else if (instr.arg == 7) stack[stackPointer] = (val1 == val2);
-                    // else if (instr.arg == 8) stack[stackPointer] = (val1 != val2);
-                    // else if (instr.arg == 9) stack[stackPointer] = (val1 < val2);
-                    // else if (instr.arg == 10) stack[stackPointer] = (val1 >= val2);
-                    // else if (instr.arg == 11) stack[stackPointer] = (val1 > val2);
-                    // else if (instr.arg == 12) stack[stackPointer] = (val1 <= val2);
+                    val2 = memory.pop();
+                    val1 = memory.pop();
+                    memory.push(val1 <= val2 ? 1 : 0);
                     break;
-                case 13:
-                    checkStackUnderflow(1);
-                    std::cout << stack[stackPointer];
-                    stackPointer--;
+                case 13: // WRITE (no newline)
+                    std::cout << memory.pop();
                     break;
-                case 14:
-                    checkStackUnderflow(1);
-                    std::cout << stack[stackPointer] << std::endl;
-                    stackPointer--;
+                case 14: // WRITELN
+                    std::cout << memory.pop() << std::endl;
                     break;
-                case 15: 
-                    checkStackOverflow(1);
-                    stack[++stackPointer] = 15;
+                case 15: // READ
+                {
+                    int inputVal;
+                    std::cin >> inputVal;
+                    memory.push(inputVal);
                     break;
+                }
                 default:
-                    throw std::runtime_error("Runtime Error: Unknown OPR code.");
+                    throw std::runtime_error("Runtime Error: Unknown OPR code " + std::to_string(instr.arg) + ".");
             }
-        } 
+        }
         else {
-            throw std::runtime_error("Runtime Error: Unknown Instruction OP: " + instr.op);
+            throw std::runtime_error("Runtime Error: Unknown instruction op: " + instr.op);
         }
     }
 }
